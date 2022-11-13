503185c8f6cd40c3974af2094459807b0369c1a0
WolfAURman <furry.wolf.alone13@gmail.com>
2022-11-13 10:07:09 +0000

    Backend: Nethunter-ify

--- anykernel.sh
+++ anykernel.sh
@@ -4,10 +4,10 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Panda
-kernel.compiler=Neutron Clang
-kernel.made=TogoFire
-message.word=Join @TogoFireChannel
+kernel.string=Sleepy-NetHunter
+kernel.compiler=Proton Clang
+kernel.made=WolfAURman
+message.word=Join @WolfAURmanRedmi9Builds
 do.devicecheck=1
 do.modules=0
 do.systemless=1
